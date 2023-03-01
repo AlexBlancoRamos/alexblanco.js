fa67778252506da71a905734df991f61ea903ac7
AlexBlancoRamos <alex.blanco@institutvidreres.cat>
2023-03-01 09:55:30 +0100

    "Metodes"

--- alexblanco.js
+++ alexblanco.js
@@ -1,6 +1,18 @@
 const cors = require('cors');
 const express = require('express');
 const app = express();
+let admin = require("firebase-admin");
+const {getFirestore} = require("firebase-admin/firestore");
+
+admin = require("firebase-admin");
+
+let serviceAccount = require("./exalexblancoramos-firebase-adminsdk-25akm-2d237b2711.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = getFirestore();
 
 
 app.use(express.json());
@@ -11,4 +23,19 @@ port = 3000;
 app.listen(port, () => {
     console.log('Server listening on the port::'+ port)
 });
+
+app.get('/jugadors',async (req, res) => {
+
+    const cityRef = db.collection('mundial').doc('final');
+    const doc = await cityRef.get();
+    if (!doc.exists) {
+        console.log('No such document!');
+    } else {
+        console.log('Document data:', doc.data());
+    }
+    res.json(doc.data());
+
+});
+
+
 
